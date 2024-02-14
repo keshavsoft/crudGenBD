9d586ee0d32bf5eb5a32f72a5fc375f873a32288
Keshav <nknnkumar@yahoo.com>
2024-02-14 11:42:29 +0530

    keshav

--- FromTableColumns/317/Transactions/Orders/repos/DeleteFuncs/EntryFile.js
+++
@@ -1,7 +0,0 @@
-import { DeleteFunc as DeleteFuncDal } from '../../dals/DeleteFuncs/EntryFile.js';
-
-let DeleteFunc = async ({ inId }) => {
-    return await DeleteFuncDal({ inId });
-};
-
-export { DeleteFunc };
